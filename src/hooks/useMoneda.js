7158a2d8f68c1f8766c721bae39a8bff079b00e7
David Caballero <jcaballeroj66@gmail.com>
2021-07-28 12:27:38 -0500

    Agregando Estilos al Custom hook

--- src/hooks/useMoneda.js
+++ src/hooks/useMoneda.js
@@ -1,5 +1,25 @@
 import React,{ Fragment, useState } from 'react';
+import styled from '@emotion/styled';
 
+const Label = styled.label`
+    font-family: 'Bebas Neue', cursive;
+    color:#fff;
+    text-transform:uppercase;
+    font-weigth:bold;
+    font-size: 2.4rem;
+    margin-top:2rem;
+    display:block;
+`;
+
+const Select = styled.select`
+    width: 100%;
+    display:block;
+    padding: 1rem;
+    -webkit-appearance: none;
+    border-radius:10px;
+    border:none;
+    font-size:1.2rem;
+`;
 const useMoneda = (label,stateInicial, opciones) => {
 
     //State para nuestro custom hook
@@ -7,15 +27,18 @@ const useMoneda = (label,stateInicial, opciones) => {
 
     const Seleccionar = () => (
         <Fragment>  
-            <label>{label}</label>
-            <select>
-                
+            <Label>{label}</Label>
+            <Select
+                onChange={e => actualizarState(e.target.value)}
+                value={state}
+            >
+
                 <option value="">-- Seleccione --</option>
                 {opciones.map(opcion => (
                     <option key={opcion.codigo} value={opcion.codigo}>{opcion.nombre}</option>
                 ))}
 
-            </select>
+            </Select>
         </Fragment>
     );
 
