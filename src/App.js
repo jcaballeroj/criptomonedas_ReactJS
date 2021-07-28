163bd2f4b033e6002fbfafd7825a2cfd01093c82
David Caballero <jcaballeroj66@gmail.com>
2021-07-28 10:48:08 -0500

    Agregando Diseño a la pagina principal

--- src/App.js
+++ src/App.js
@@ -1,7 +1,53 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import imagen from './cryptomonedas.png';
+const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  @media (min-width:992px){
+    display:grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`;
+const Imagen = styled.img`
+  max-width: 100%;
+  margin-top:5rem;
+
+`;
+
+const Heading = styled.h1`
+  font-family: 'Bebas Neue',cursive;
+  color: #ffff;
+  text-aling: left;
+  font-weigth:700;
+  font-size:50px;
+  margin-bottom: 50px;
+  margin-top:80px;
+  
+    &::after {
+      content: '';
+      width: 100px;
+      height: 6px;
+      background-color: #66A2FE;
+      display:block;
+    }
+
+`;
+
 function App() {
   return (
-    <h1>Cotizador de Criptomonedas</h1>
+    <Container>
+      <div>
+        <Imagen 
+          src={imagen}
+          alt="imagen Cripto"
+        />
+      </div>
+      <div>
+        <Heading>Cotiza Criptomonedas al Instante</Heading>
+      </div>
+    </Container>
   );
 }
 
